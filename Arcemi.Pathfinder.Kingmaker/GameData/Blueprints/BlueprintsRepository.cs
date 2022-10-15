cbe30384f7e139d5a804f2172790825a714534b3
Dixon, Evan <evanldixon@gmail.com>
2022-10-15 13:25:00 -0500

    Improve speed by always using zip hack and not letting exceptions be thrown

--- Arcemi.Pathfinder.Kingmaker/GameData/Blueprints/BlueprintsRepository.cs
+++ Arcemi.Pathfinder.Kingmaker/GameData/Blueprints/BlueprintsRepository.cs
@@ -57,49 +57,27 @@ namespace Arcemi.Pathfinder.Kingmaker.GameData.Blueprints
                 Stream entryStream = null;
                 try
                 {
-                    bool useHack = false;
-                    try
-                    {
-                        entryStream = entry.Open();
+                    // WOTR ships with a slightly corrupt zip file that can still be read if we try hard enough
+                    // Seems to be a result of missing the zip64 header that specifies compressed size, while having a zip32 compressed size indicating to use zip64
 
-                        using var streamReader = new StreamReader(entryStream);
-                        blueprintContent = await streamReader.ReadToEndAsync();
-                        if (string.IsNullOrEmpty(blueprintContent))
-                        {
-                            useHack = true;
-                        }
-                    }
-                    catch (InvalidDataException)
-                    {
-                        useHack = true;
-                    }
+                    // Some entries have a compressed size that's too small
+                    // .Net uses this as an upper bound for the data it can read, and increasing it lets us read up to the actual end of the file,
+                    // but it also uses it as validation to prevent reading a file that extends beyond the end of the file
+                    entry.GetType()
+                        .GetField("_compressedSize", BindingFlags.NonPublic | BindingFlags.Instance)
+                        .SetValue(entry, uint.MaxValue);
 
-                    if (useHack)
-                    {
-                        entryStream?.Dispose();
+                    // ZipArchiveEntry.Open checks _compressedSize to ensure it doesn't extend beyond the end of the zip file.
+                    // Some entries are like this already, others are only like this because we increased it to fix a different issue.
+                    // ZipArchiveEntry contains a different method that lets us bypass errors related to size, but it's private.
+                    // ZipArchiveEntry.OpenInReadMode(bool checkOpenable). https://github.com/dotnet/runtime/blob/2d8f10528f91461344ede350f6a37283c87d581d/src/libraries/System.IO.Compression/src/System/IO/Compression/ZipArchiveEntry.cs#L681
+                    entryStream = (Stream)entry
+                        .GetType()
+                        .GetMethod("OpenInReadMode", BindingFlags.NonPublic | BindingFlags.Instance, new[] { typeof(bool) })
+                        .Invoke(entry, new object[] { false /* checkOpenable */ });
 
-                        // WOTR ships with a slightly corrupt zip file that can still be read if we try hard enough
-                        // Seems to be a result of missing the zip64 header that specifies compressed size, while having a zip32 compressed size indicating to use zip64
-
-                        // Some entries have a compressed size that's too small
-                        // .Net uses this as an upper bound for the data it can read, and increasing it lets us read up to the actual end of the file
-                        // but it also uses it as validation to prevent reading a file that extends beyond the end of the file
-                        entry.GetType()
-                            .GetField("_compressedSize", BindingFlags.NonPublic | BindingFlags.Instance)
-                            .SetValue(entry, uint.MaxValue);
-
-                        // ZipArchiveEntry contains a method that lets us bypass errors related to size, but it's private
-                        // OpenInReadMode(bool checkOpenable)
-                        // https://github.com/dotnet/runtime/blob/2d8f10528f91461344ede350f6a37283c87d581d/src/libraries/System.IO.Compression/src/System/IO/Compression/ZipArchiveEntry.cs#L681
-                        entryStream = (Stream)entry
-                            .GetType()
-                            .GetMethod("OpenInReadMode", BindingFlags.NonPublic | BindingFlags.Instance, new[] { typeof(bool) })
-                            .Invoke(entry, new object[] { false /* checkOpenable */ });
-
-
-                        using var streamReader = new StreamReader(entryStream);
-                        blueprintContent = await streamReader.ReadToEndAsync();
-                    }
+                    using var streamReader = new StreamReader(entryStream);
+                    blueprintContent = await streamReader.ReadToEndAsync();
                 }
                 finally
                 {
